4d516c33974052d9a77560d1ffa4254dfa57181d
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-06-25 22:27:20 +0800

    fetch every page of webinar registrants
    `get_registrants_for_selected_webinars.py`:
    - the previous version of `get_registrants_for_selected_webinars.py` only
      fetched the first page of webinar registrants, which seems to be
      limited to only 25 registrants
      - now, the script fetches successive pages of registrants until there
        are none left
    - more professional-looking info messages (the ones done with `print()`)
    - exclude certain webinars if they contained the year '2025' in the name
      or schedule
      - moved this variable and the previous variable containing '2026' to
        `config.toml`
    - capitalised all config variable names
    - reduced the sleep time in between API calls while still avoiding hitting
      limits

--- get_registrants_for_selected_webinars.py
+++ get_registrants_for_selected_webinars.py
@@ -15,44 +15,64 @@ API_KEY = os.environ["WEBINARJAM_API_KEY"]
 with open("config.toml", "rb") as file:
     config = tomllib.load(file)
 
-output_directory_path = Path(config["output_folder_path"])
+OUTPUT_DIRECTORY_PATH = Path(config["output_folder_path"])
 
-webinar_list_filename = config["get_all_webinars"]["output_filename"]
-webinar_list_file_path = output_directory_path / webinar_list_filename
+WEBINARS_LIST_FILENAME = config["get_all_webinars"]["output_filename"]
+WEBINARS_LIST_FILE_PATH = OUTPUT_DIRECTORY_PATH / WEBINARS_LIST_FILENAME
 
-output_filename = config["get_registrants_for_selected_webinars"]["output_filename"]
-output_file_path = output_directory_path / output_filename
-
-print(f"reading the list of all webinars from {webinar_list_file_path}...\n")
-
-with open(webinar_list_file_path) as file:
+with open(WEBINARS_LIST_FILE_PATH) as file:
     webinars = json.load(file)["webinars"]
 
-endpoint = config["get_registrants_for_selected_webinars"]["api_endpoint"]
+print(f"Loaded the list of all webinars from {WEBINARS_LIST_FILE_PATH}.")
+print()
 
-year = "2026"
+ENDPOINT = config["get_registrants_for_selected_webinars"]["api_endpoint"]
+
+INCLUDE_YEAR = config["get_registrants_for_selected_webinars"]["include_year"]
+EXCLUDE_YEAR = config["get_registrants_for_selected_webinars"]["exclude_year"]
+
 registrants = {}
 
 for webinar in webinars:
-    if year in webinar["name"] or year in webinar["schedules"][0]:
-        webinar_id = webinar["webinar_id"]
+    webinar_name = webinar["name"]
+    webinar_id = webinar["webinar_id"]
 
-        request_data = {
-            "api_key": API_KEY,
-            "webinar_id": webinar_id
-        }
-        print(f"getting registrants for webinar '{webinar["name"]}' with id {webinar_id}...")
+    if (INCLUDE_YEAR in webinar_name or INCLUDE_YEAR in webinar["schedules"][0]) and (EXCLUDE_YEAR not in webinar_name and EXCLUDE_YEAR not in webinar["schedules"][0]):
+        current_page = 1
+        registrants_of_current_webinar = []
 
-        response = requests.post(endpoint, request_data)
+        # the WebinarJam API does not return all registrants at once, but instead splits them into pages
+        # which means that a new POST request has to be sent for each page
+        while True:
+            print(f"[page {current_page}] Fetching registrants for webinar '{webinar_name}' (ID: {webinar_id}) ...")
 
-        response_json = response.json()
-        registrants[webinar_id] = response_json["registrants"]["data"]
+            request_data = {
+                "api_key": API_KEY,
+                "webinar_id": webinar_id,
+                "page": current_page
+            }
 
-        # prevent exceeding the WebinarJam API's limit of 20 API calls per second
-        time.sleep(0.1)
+            response = requests.post(ENDPOINT, request_data)
+
+            response_json = response.json()
+            registrants_of_current_webinar += response_json["registrants"]["data"]
+
+            # prevent exceeding the WebinarJam API's limit of 20 API calls per second
+            time.sleep(0.06)
+
+            if (response_json["registrants"]["next_page_url"] is None):
+                break
+
+            current_page += 1
+
+        registrants[webinar_id] = registrants_of_current_webinar
+        print()
+
+output_filename = config["get_registrants_for_selected_webinars"]["output_filename"]
+output_file_path = OUTPUT_DIRECTORY_PATH / output_filename
 
 with open(output_file_path, "w") as file:
     formatted_registrants = json.dumps(registrants, indent=4)
     file.write(formatted_registrants)
 
-print("\nsuccessfully written the registrant data of", len(registrants), "webinars to", output_file_path)
+print(f"Successfully written the registrant data of {len(registrants)} webinars to {output_file_path}.")
