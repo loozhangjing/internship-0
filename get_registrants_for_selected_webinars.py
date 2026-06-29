42e7feb8e6e1ccc4ff1cefd26de6ec61c1b71978
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-06-29 17:27:15 +0800

    more filters for webinars to fetch registrants for
    - exclude webinars with names containing certain words
    - parse the date the webinar was first held from its schedules property
      instead of checking a year string (e.g. "2026") against the webinar
      name and schedule
      - since the check is now done using Python `datetime.year` values,
        year filters are 100% accurate (except when the date cannot be
        parsed, in which case the webinar is skipped)

--- get_registrants_for_selected_webinars.py
+++ get_registrants_for_selected_webinars.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import json
+from datetime import datetime
 import time
 
 from dotenv import load_dotenv
@@ -17,19 +18,25 @@ with open("config.toml", "rb") as file:
 
 OUTPUT_DIRECTORY_PATH = Path(config["output_folder_path"])
 
-WEBINARS_LIST_FILENAME = config["get_all_webinars"]["output_filename"]
-WEBINARS_LIST_FILE_PATH = OUTPUT_DIRECTORY_PATH / WEBINARS_LIST_FILENAME
+WEBINARS_LIST_FILE_PATH = OUTPUT_DIRECTORY_PATH / config["get_all_webinars"]["output_filename"]
 
 with open(WEBINARS_LIST_FILE_PATH) as file:
     webinars = json.load(file)["webinars"]
 
-print(f"Loaded the list of all webinars from {WEBINARS_LIST_FILE_PATH}.")
+webinar_count = len(webinars)
+
+print(f"Loaded information for all {webinar_count} webinars from {WEBINARS_LIST_FILE_PATH}.")
 print()
 
 ENDPOINT = config["get_registrants_for_selected_webinars"]["api_endpoint"]
 
 INCLUDE_YEAR = config["get_registrants_for_selected_webinars"]["include_year"]
-EXCLUDE_YEAR = config["get_registrants_for_selected_webinars"]["exclude_year"]
+EXCLUDED_KEYWORDS = config["get_registrants_for_selected_webinars"]["exclude_webinars_with_names_containing"]
+
+# (see https://docs.python.org/3/library/datetime.html#format-codes)
+# the time string in the registrant data looks like this:
+# Tuesday, 17 Mar 2026, 4:00 PM
+STRPTIME_FORMAT = "%A, %d %b %Y, %I:%M %p"
 
 registrants = {}
 
@@ -37,36 +44,61 @@ for webinar in webinars:
     webinar_name = webinar["name"]
     webinar_id = webinar["webinar_id"]
 
-    if (INCLUDE_YEAR in webinar_name or INCLUDE_YEAR in webinar["schedules"][0]) and (EXCLUDE_YEAR not in webinar_name and EXCLUDE_YEAR not in webinar["schedules"][0]):
-        current_page = 1
-        registrants_of_current_webinar = []
+    # there could be one or multiple schedules for a single webinar
+    webinar_first_schedule = webinar["schedules"][0]
 
-        # the WebinarJam API does not return all registrants at once, but instead splits them into pages
-        # which means that a new POST request has to be sent for each page
-        while True:
-            print(f"[page {current_page}] Fetching registrants for webinar '{webinar_name}' (ID: {webinar_id}) ...")
+    try:
+        webinar_first_schedule_datetime = datetime.strptime(webinar_first_schedule, STRPTIME_FORMAT)
+    except ValueError:
+        # some webinars will have a schedule of 'Right now', in which case there is no other data that can be used to determine the webinar's date
+        print(f"Skipped webinar '{webinar_name}' because its date '{webinar_first_schedule}' is unparseable.")
+        continue
 
-            request_data = {
-                "api_key": API_KEY,
-                "webinar_id": webinar_id,
-                "page": current_page
-            }
+    # skip webinars that are not in the specified year
+    if webinar_first_schedule_datetime.year != INCLUDE_YEAR:
+        print(f"Skipped webinar '{webinar_name}' because it was not held in the year {INCLUDE_YEAR}.")
+        continue
 
-            response = requests.post(ENDPOINT, request_data)
+    # a `continue` inside this `for` loop has no effect on the outer `for webinar in webinars` loop
+    skip = False
+    for keyword in EXCLUDED_KEYWORDS:
+        # check the keyword against the webinar name in lowercase
+        if keyword in webinar_name.lower():
+            print(f"Skipped webinar '{webinar_name}' because its name contains '{keyword}'.")
+            skip = True
+    if skip is True:
+        continue
 
-            response_json = response.json()
-            registrants_of_current_webinar += response_json["registrants"]["data"]
+    current_page = 1
+    registrants_of_current_webinar = []
 
-            # prevent exceeding the WebinarJam API's limit of 20 API calls per second
-            time.sleep(0.06)
+    # the WebinarJam API does not return all registrants at once, but instead splits them into pages
+    # which means that a new POST request has to be sent for each page
+    print()
+    while True:
+        print(f"[page {current_page}] Fetching registrants for webinar '{webinar_name}' (ID: {webinar_id})...")
 
-            if (response_json["registrants"]["next_page_url"] is None):
-                break
+        request_data = {
+            "api_key": API_KEY,
+            "webinar_id": webinar_id,
+            "page": current_page
+        }
 
-            current_page += 1
+        response = requests.post(ENDPOINT, request_data)
 
-        registrants[webinar_id] = registrants_of_current_webinar
-        print()
+        response_json = response.json()
+        registrants_of_current_webinar += response_json["registrants"]["data"]
+
+        # prevent exceeding the WebinarJam API's limit of 20 API calls per second
+        time.sleep(0.06)
+
+        if (response_json["registrants"]["next_page_url"] is None):
+            break
+
+        current_page += 1
+
+    registrants[webinar_id] = registrants_of_current_webinar
+    print()
 
 output_filename = config["get_registrants_for_selected_webinars"]["output_filename"]
 output_file_path = OUTPUT_DIRECTORY_PATH / output_filename
@@ -75,4 +107,5 @@ with open(output_file_path, "w") as file:
     formatted_registrants = json.dumps(registrants, indent=4)
     file.write(formatted_registrants)
 
+print()
 print(f"Successfully written the registrant data of {len(registrants)} webinars to {output_file_path}.")
