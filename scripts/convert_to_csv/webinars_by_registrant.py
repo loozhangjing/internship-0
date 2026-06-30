564d8c71cb458f04da6a634367ecf44dcf3fe1ef
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-06-30 15:13:15 +0800

    rename column names using pandas

--- scripts/convert_to_csv/webinars_by_registrant.py
+++ scripts/convert_to_csv/webinars_by_registrant.py
@@ -14,7 +14,16 @@ WEBINARS_BY_REGISTRANT_FILE_PATH = OUTPUT_DIRECTORY_PATH / config["transform_int
 OUTPUT_FILE_PATH = OUTPUT_DIRECTORY_PATH / Path(config["convert_webinars_by_registrant_to_csv"]["output_filename"])
 
 with open(WEBINARS_BY_REGISTRANT_FILE_PATH) as file:
-    registrants = pd.read_json(file).transpose()
+    registrants_df = pd.read_json(file).transpose()
+
+registrants_df = registrants_df.rename(columns = {
+    "first_name": "First name",
+    "last_name": "Last name",
+    "phone_country_code": "Phone country code",
+    "phone_number": "Phone number",
+    "total_registered": "Total registered webinars",
+    "total_attended": "Total attended webinars",
+})
 
 with open(OUTPUT_FILE_PATH, "w") as file:
-    file.write(registrants.to_csv())
+    file.write(registrants_df.to_csv())
