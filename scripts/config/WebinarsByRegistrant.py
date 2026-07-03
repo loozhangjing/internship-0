1dc42a7bc66f84a3b30b9f60ba1b77e8527cfb4d
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-03 15:48:30 +0800

    WebinarsByRegistrantConfig & drop irrelevant cols

---
+++ scripts/config/WebinarsByRegistrant.py
@@ -0,0 +1,10 @@
+class WebinarsByRegistrantConfig:
+    RELEVANT_COLUMNS = [
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "webinar",
+        "webinar_id",
+        "attended_live"
+    ]
--- scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
+++ scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 from registrants_by_webinar_ids import registrants_by_webinar_ids
+from config.WebinarsByRegistrant import WebinarsByRegistrantConfig
 
 registrants_for_selected_webinars = registrants_by_webinar_ids([1228, 1229])
 
 df = pd.DataFrame(registrants_for_selected_webinars)
+
+# select only relevant columns so the other columns are dropped (removed)
+df = df[WebinarsByRegistrantConfig.RELEVANT_COLUMNS]
 
 # pretty-print the `DataFrame` (pipe the output to less -S for scrolling)
 with pd.option_context({ "display.max_rows": None, "display.max_columns": None, "display.width": None }):
