162e7a3dd0f44c955a1d5978f91e6e04cda0f273
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-03 15:56:30 +0800

    log the number of unique registrants & webinars

--- scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
+++ scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
@@ -1,7 +1,11 @@
+import logging
+
 import pandas as pd
 
 from registrants_by_webinar_ids import registrants_by_webinar_ids
 from config.WebinarsByRegistrant import WebinarsByRegistrantConfig
+
+logging.basicConfig(level=logging.INFO)
 
 registrants_for_selected_webinars = registrants_by_webinar_ids([1228, 1229])
 
@@ -10,6 +14,7 @@ df = pd.DataFrame(registrants_for_selected_webinars)
 # select only relevant columns so the other columns are dropped (removed)
 df = df[WebinarsByRegistrantConfig.RELEVANT_COLUMNS]
 
-# pretty-print the `DataFrame` (pipe the output to less -S for scrolling)
-with pd.option_context({ "display.max_rows": None, "display.max_columns": None, "display.width": None }):
-    print(df)
+unique_email_count = len(df.email.unique())
+webinar_count = len(df.webinar_id.unique())
+
+logging.info(f"there are {unique_email_count} unique registrants (identified by their emails) across {webinar_count} webinars")
