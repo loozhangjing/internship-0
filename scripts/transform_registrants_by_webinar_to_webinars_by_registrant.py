cd815bd0a43a3ceb89a6462164eaa7dab6da4e7a
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-03 18:35:07 +0800

    transform into webinars by registrant
    turns this:
    | email             | webinar_id | attended_live | ... |
    | ----------------- | ---------- | ------------- | --- |
    | example@gmail.com | 1228       | No            | ... |
    | example@gmail.com | 1229       | Yes           | ... |
    
    into this (using `.apply()` with a function that adds a column for every
    webinar, using the webinar ID as the label for that column)
    | 1228       | 1229     | email             | webinar_id | attended_live | ... |
    | ---------- | -------- | ----------------- | ---------- | ------------- | --- |
    | registered | NaN      | example@gmail.com | 1228       | No            | ... |
    | NaN        | attended | example@gmail.com | 1229       | Yes           | ... |
    
    then into this (using `.groupby("email")`, which combines every row with
    the same email into one row, then `.first()`, which takes the first
    value of every column
    | email             | 1228       | 1229     | webinar_id | attended_live | ... |
    | ----------------- | ---------- | -------- | ---------- | ------------- | --- |
    | example@gmail.com | registered | attended | 1228       | No            | ... |

--- scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
+++ scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
@@ -9,12 +9,30 @@ logging.basicConfig(level=logging.INFO)
 
 registrants_for_selected_webinars = registrants_by_webinar_ids([1228, 1229])
 
-df = pd.DataFrame(registrants_for_selected_webinars)
+registrants_by_webinar = pd.DataFrame(registrants_for_selected_webinars)
 
 # select only relevant columns so the other columns are dropped (removed)
-df = df[WebinarsByRegistrantConfig.RELEVANT_COLUMNS]
+registrants_by_webinar = registrants_by_webinar[WebinarsByRegistrantConfig.RELEVANT_COLUMNS]
 
-unique_email_count = len(df.email.unique())
-webinar_count = len(df.webinar_id.unique())
+unique_emails = registrants_by_webinar.email.unique()
+webinar_ids = registrants_by_webinar.webinar_id.unique()
 
-logging.info(f"there are {unique_email_count} unique registrants (identified by their emails) across {webinar_count} webinars")
+logging.info(f"there are {len(unique_emails)} unique registrants (identified by their emails) across {len(webinar_ids)} webinars")
+
+def add_a_column_for_every_unique_webinar(row):
+    new_column_name = str(row.webinar_id)
+
+    if row.attended_live == "Yes":
+        row[new_column_name] = "attended"
+    else:
+        row[new_column_name] = "registered"
+    return row
+
+registrants_by_webinar = registrants_by_webinar.apply(add_a_column_for_every_unique_webinar, axis="columns")
+
+# combines all rows with the same email
+# for every column's value, take the first value that's not NaN
+registrants_by_webinar = registrants_by_webinar.groupby("email").first()
+
+with pd.option_context({ "display.max_rows": None, "display.max_columns": None, "display.width": None }):
+    print(registrants_by_webinar)
