fee74c3d1fa58c86b1ef457d57e3a6c32b1ec501
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-06 17:09:45 +0800

    function that returns webinars matching criteria
    - the file is in the newly created `scripts/functions/` directory
    - the function can search for webinars in `webinar_list.json` that were
      held in certain years (it ignores webinars with an unparseable
      `schedule[0]`) and with names that contain certain strings

---
+++ scripts/functions/webinar_ids_by_query.py
@@ -0,0 +1,54 @@
+import logging
+import json
+from datetime import datetime
+from typing import List
+
+from config.GlobalConfig import GlobalConfig
+from config.WebinarListConfig import WebinarListConfig
+
+logging.basicConfig(level=logging.DEBUG)
+
+def webinar_ids_by_query(
+        included_years: List[int],
+        names_must_include_one_of: List[str]
+):
+    WEBINAR_LIST_FILE_PATH = (
+        GlobalConfig.OUTPUT_DIRECTORY_PATH
+        / WebinarListConfig.OUTPUT_FILENAME
+    )
+
+    with open(WEBINAR_LIST_FILE_PATH) as file:
+        webinar_list = json.load(file)
+
+    matching_webinar_ids = []
+
+    for webinar in webinar_list:
+        id = webinar["webinar_id"]
+        name = webinar["name"]
+        date_str = webinar["schedules"][0]
+
+        try:
+            webinar_datetime = datetime.strptime(
+                date_str,
+                WebinarListConfig.STRPTIME_FORMAT
+            )
+        except ValueError:
+            logging.error(
+                f"date '{date_str}' for the webinar '{name}' "
+                f"could not be parsed, skipping webinar '{name}'"
+            )
+            continue
+
+        for year in included_years:
+            if webinar_datetime.year == year:
+                for string in names_must_include_one_of:
+                    string = string.lower()
+                    if string in name.lower():
+                        logging.debug(
+                            f"webinar '{name}' was held in the year {year} "
+                            f"and has '{string}' in its name, "
+                            f"adding webinar {id} to the list"
+                        )
+                        matching_webinar_ids.append(id)
+
+    return matching_webinar_ids
