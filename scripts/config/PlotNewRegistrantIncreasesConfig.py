340d10dea2fbe0ef02b2de61d5062b9136ae6eec
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-07 20:32:06 +0800

    plot the new unique registrants per webinar
    - using `matplotlib`,
        - the x-axis represents the number of new unique registrants (i.e.
          emails that did not join any webinar held previously)
        - the y-axis represents all the free webinars held in 2026, sorted
          with the earliest ones at the bottom and the most r	cent ones at
          the top

--- scripts/config/PlotNewRegistrantIncreasesConfig.py
+++ scripts/config/PlotNewRegistrantIncreasesConfig.py
@@ -2,4 +2,6 @@ class PlotNewRegistrantIncreasesConfig:
     WEBINAR_MUSTVE_BEEN_HELD_IN_THE_YEARS = [2026]
     WEBINAR_NAMES_MUST_INCLUDE_ONE_OF = ["free", "免费"]
 
+    STRPTIME_FORMAT = "%a, %d %b %Y, %I:%M %p"
+    STRFTIME_FORMAT = "%d/%m/%Y"
     OUTPUT_SUBDIRECTORY = "plots/"
--- scripts/plot_new_registrant_increases.py
+++ scripts/plot_new_registrant_increases.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+import re
 import logging
 
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 
+from config.GlobalConfig import GlobalConfig
 from config.PlotNewRegistrantIncreasesConfig\
     import PlotNewRegistrantIncreasesConfig
 
@@ -25,29 +31,65 @@ df = df.iloc[:, ::-1]
 
 grouped = df.groupby("webinar_id")
 
-new_registrants = {}
+new_registrants_dict = {}
 encountered_emails = []
 
-for (webinar_id, webinar_df) in grouped:
+for (_webinar_id, webinar_df) in grouped:
+    webinar_name = webinar_df.loc[:, "webinar"].iloc[0].strip()
+
+    webinar_datetime = datetime.strptime(
+        webinar_df.loc[:, "schedule"].iloc[0],
+        PlotNewRegistrantIncreasesConfig.STRPTIME_FORMAT
+    ).strftime(PlotNewRegistrantIncreasesConfig.STRFTIME_FORMAT)
+
+    # remove text inside the first occurence of brackets (between "(" and ")")
+    webinar_name = re.sub("^\\(.*?\\)", "", webinar_name)
+    webinar_name = f"[{webinar_datetime}] {webinar_name}"
+
     new_emails = []
 
     for email in webinar_df.loc[:, "email"]:
         if email not in encountered_emails:
-            if webinar_id not in new_registrants:
-                new_registrants[webinar_id] = 0
-            new_registrants[webinar_id] += 1
+            if webinar_name not in new_registrants_dict:
+                new_registrants_dict[webinar_name] = 0
+            new_registrants_dict[webinar_name] += 1
             new_emails.append(email)
 
     logger.debug(
         f"there are {webinar_df.shape[0]} registrants "
-        f"for webinar {webinar_id}, "
+        f"for webinar '{webinar_name}', "
         f"with {len(new_emails)} of them being new"
     )
 
     encountered_emails.extend(new_emails)
 
-logging.info(
-    "number of new registrants per webinar: "
-    "\n"
-    f"{new_registrants}"
+# render Chinese characters properly
+matplotlib.rc("font", family="Noto Sans CJK SC")
+
+fig, ax = plt.subplots()
+
+new_registrant_counts = list(new_registrants_dict.values())
+webinar_names = list(new_registrants_dict.keys())
+
+webinar_count = len(webinar_names)
+
+ax.set_xlabel("Number of new unique registrants")
+ax.set_ylabel("Webinar")
+
+ax.plot(new_registrant_counts, np.arange(webinar_count))
+
+ax.set_yticks(
+    list(n for n in range(webinar_count)),
+    labels=webinar_names
 )
+
+OUTPUT_PATH = (
+    GlobalConfig.OUTPUT_DIRECTORY_PATH
+    / PlotNewRegistrantIncreasesConfig.OUTPUT_SUBDIRECTORY
+)
+
+OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
+
+# `bbox_inches="tight"` prevents the axis labels from overflowing the screen
+# and being hidden
+plt.savefig(OUTPUT_PATH / "plot.pdf", bbox_inches="tight")
