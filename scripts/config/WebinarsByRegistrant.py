28ea351dc0447f4e4f557682cc43e974417fcc2b
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-08 16:32:20 +0800

    format `WebinarsByRegistrant` config
    - split some lines into multiple to comply with the PEP 8 line length
    - made some comments clearer
    - changed the `format_phone_number()` function to remove '60' from the
      beginning of phone numbers that are 11 or 12 characters long, instead
      of the previous condition that it had to be more than 11 characters
      long (which would not apply to phone numbers such as '60 12 345 6789'
      when it should've)

--- scripts/config/WebinarsByRegistrant.py
+++ scripts/config/WebinarsByRegistrant.py
@@ -4,10 +4,19 @@ import json
 from config.GlobalConfig import GlobalConfig
 from config.WebinarListConfig import WebinarListConfig
 
-with open(GlobalConfig.OUTPUT_DIRECTORY_PATH / WebinarListConfig.OUTPUT_FILENAME) as file:
+WEBINAR_LIST_PATH = (
+    GlobalConfig.OUTPUT_DIRECTORY_PATH
+    / WebinarListConfig.OUTPUT_FILENAME
+)
+
+with open(WEBINAR_LIST_PATH) as file:
     webinar_list = json.load(file)
 
-    webinar_ids_to_names =  {str(webinar["webinar_id"]): webinar["name"] for webinar in webinar_list}
+    # create a mapping of numeric webinar IDs to the webinar names, based on
+    # the data in the file specified in `WebinarListConfig.OUTPUT_FILENAME`
+    webinar_ids_to_names =  {
+        str(webinar["webinar_id"]): webinar["name"] for webinar in webinar_list
+    }
 
 class WebinarsByRegistrantConfig:
     OUTPUT_FILENAME = "webinars_by_registrant.csv"
@@ -20,7 +29,7 @@ class WebinarsByRegistrantConfig:
         "1176", "1177",
     ]
 
-    # should be passed to the `.apply()` method of a `DataFrame`
+    # should be passed to the `.apply()` method of a `pandas.DataFrame`
     @staticmethod
     def format_row(row):
         row.first_name = row.first_name.title()
@@ -40,7 +49,8 @@ class WebinarsByRegistrantConfig:
 
     webinar_ids_to_names = webinar_ids_to_names
 
-    # labels of the columns to keep (all other columns will be deleted) for the data loaded from the `.json` files, i.e. the registrants by webinar
+    # labels of the columns to keep (all other columns will be deleted) for
+    # the 'registrants by webinar' data loaded from the `.json` files
     RELEVANT_COLUMNS_BEFORE_GROUPING = [
         "email",
         "first_name",
@@ -50,24 +60,40 @@ class WebinarsByRegistrantConfig:
         "attended_live"
     ]
 
-    # labels of the columns to remove after every row with the same email has been combined, i.e. the data in webinars by registrant form
+    # labels of the columns to remove after every row with the same email has
+    # been combined, i.e. the 'registrants by webinar' data has been
+    # converted to a 'webinars by registrant' format
     COLUMNS_TO_DROP_AFTER_GROUPING = [
         "webinar_id",
         "attended_live"
     ]
 
 def format_phone_number(phone_number):
-    # make phone numbers numeric-only (remove extraneous characters like '-')
-    # \D in a regular expression means any digit
+    """
+    Examples:
+    format_phone_number("012 345678")
+    # -> "012 345 678"
+    format_phone_number("+60123456789")
+    # -> "012 345 6789"
+    format_phone_number("03123456")
+    # -> "03 123456"
+    """
+
+    # make the phone number numeric-only
+    # i.e. remove extraneous characters like '-'
+    # ('\D' in a regular expression means any digit)
     phone_number_digits_only = re.sub(r"\D", "", str(phone_number))
 
-    # convert them to integers (to remove any trailing/leading zeroes)
-    phone_number_int = int(phone_number_digits_only)
+    # convert the phone number to an integer, then back to a string
+    # to remove any trailing/leading zeroes
+    formatted_phone_number = str(int(phone_number_digits_only))
 
-    formatted_phone_number = str(phone_number_int)
-
-    # a phone number beginning with "60" will not have been truncated by turning it into an int
-    if formatted_phone_number[:2] == "60" and len(formatted_phone_number) > 11:
+    # if the phone number begins with a '60',
+    # and its length is that of a home phone number,
+    # (60 xx xxx xxxx) or (60 xx xxxx xxxx)
+    # remove the '60'
+    if formatted_phone_number[:2] == "60"\
+    and len(formatted_phone_number) == 11 or len(formatted_phone_number) == 12:
         formatted_phone_number = formatted_phone_number[2:]
 
     formatted_phone_number = "0" + formatted_phone_number
@@ -79,9 +105,16 @@ def format_phone_number(phone_number):
         if len(formatted_phone_number) == 11:
             second_section_length = 4
 
-        formatted_phone_number = formatted_phone_number[:3] + " " + formatted_phone_number[3:second_section_length + 3] + " " + formatted_phone_number[second_section_length + 3:]
+        formatted_phone_number = (
+            f"{formatted_phone_number[:3]} "
+            f"{formatted_phone_number[3:second_section_length + 3]} "
+            f"{formatted_phone_number[second_section_length + 3:]}"
+        )
     else:
         # for phone numbers that don't begin with "01" (e.g. 03 xxxxxxx)
-        formatted_phone_number = formatted_phone_number[:2] + " " + formatted_phone_number[2:]
+        formatted_phone_number = (
+            f"{formatted_phone_number[:2]} "
+            f"{formatted_phone_number[2:]}"
+        )
 
     return formatted_phone_number
