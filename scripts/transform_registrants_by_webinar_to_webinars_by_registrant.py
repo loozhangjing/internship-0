3084d52eb88ccb41cb8109f2b49d971ec9f70b17
Loo Zhang Jing <zzzhangjinggg@gmail.com>
2026-07-08 16:00:03 +0800

    format transform_registrants_by_webinar_to_webinars_by_registrant.py
    - split long lines into multiple lines to comply with PEP 8
    - changed the logging a bit

--- scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
+++ scripts/transform_registrants_by_webinar_to_webinars_by_registrant.py
@@ -8,15 +8,23 @@ from config.WebinarsByRegistrant import WebinarsByRegistrantConfig
 
 logging.basicConfig(level=logging.INFO)
 
-registrants_for_selected_webinars = registrants_by_webinar_ids(WebinarsByRegistrantConfig.WEBINAR_IDS)
+registrants_for_selected_webinars = registrants_by_webinar_ids(
+    WebinarsByRegistrantConfig.WEBINAR_IDS
+)
 
 registrants_by_webinar = pd.DataFrame(registrants_for_selected_webinars)
 
 # select only relevant columns so the other columns are dropped (removed)
-registrants_by_webinar = registrants_by_webinar[WebinarsByRegistrantConfig.RELEVANT_COLUMNS_BEFORE_GROUPING]
+registrants_by_webinar = registrants_by_webinar[
+    WebinarsByRegistrantConfig.RELEVANT_COLUMNS_BEFORE_GROUPING
+]
 
-logging.info("")
-logging.info(f"there are {len(registrants_by_webinar.email.unique())} unique registrants (identified by their emails) across {len(WebinarsByRegistrantConfig.WEBINAR_IDS)} webinars")
+logging.info(
+    f"there are {len(registrants_by_webinar.email.unique())} "
+    f"unique registrants (as identified by their emails) "
+    f"across {len(WebinarsByRegistrantConfig.WEBINAR_IDS)} webinars"
+    "\n"
+)
 
 def add_a_column_for_every_unique_webinar(row):
     new_column_name = str(row.webinar_id)
@@ -27,7 +35,10 @@ def add_a_column_for_every_unique_webinar(row):
         row[new_column_name] = "registered"
     return row
 
-registrants_by_webinar = registrants_by_webinar.apply(add_a_column_for_every_unique_webinar, axis="columns")
+registrants_by_webinar = registrants_by_webinar.apply(
+    add_a_column_for_every_unique_webinar,
+    axis="columns"
+)
 
 # combines all rows with the same email
 # for every column's value, take the first value that's not NaN
@@ -40,15 +51,27 @@ webinars_by_registrant = registrants_by_webinar.drop(
 )
 
 def add_columns_for_total_webinars_joined(row):
-    row["total_attended"] = sum(1 for value in row[WebinarsByRegistrantConfig.WEBINAR_IDS] if value == "attended")
-    row["total_registered"] = sum(1 for value in row[WebinarsByRegistrantConfig.WEBINAR_IDS] if value == "registered" or value == "attended")
+    row["total_attended"] = sum(
+        1 for value in row[WebinarsByRegistrantConfig.WEBINAR_IDS]
+        if value == "attended"
+    )
+    row["total_registered"] = sum(
+        1 for value in row[WebinarsByRegistrantConfig.WEBINAR_IDS]
+        if value == "registered" or value == "attended"
+    )
 
     return row
 
-webinars_by_registrant = webinars_by_registrant.apply(add_columns_for_total_webinars_joined, axis="columns")
+webinars_by_registrant = webinars_by_registrant.apply(
+    add_columns_for_total_webinars_joined,
+    axis="columns"
+)
 
 # move the columns representing webinars to the end
-columns_that_come_first = [label for label in webinars_by_registrant.columns.tolist() if label not in WebinarsByRegistrantConfig.WEBINAR_IDS] # non-webinar-ID column labels
+columns_that_come_first = [
+    label for label in webinars_by_registrant.columns.tolist()
+    if label not in WebinarsByRegistrantConfig.WEBINAR_IDS
+] # non-webinar-ID column labels
 webinars_by_registrant = pd.concat(
     [
         webinars_by_registrant.loc[:, columns_that_come_first],
@@ -72,14 +95,18 @@ webinars_by_registrant.rename(
     inplace = True
 )
 
-# rename column labels of numeric webinar IDs into the corresponding webinar names
+# rename the column labels that are numeric webinar IDs
+# into their corresponding webinar names
 webinars_by_registrant.rename(
     WebinarsByRegistrantConfig.webinar_ids_to_names,
     axis = "columns",
     inplace = True
 )
 
-OUTPUT_FILE_PATH = GlobalConfig.OUTPUT_DIRECTORY_PATH / WebinarsByRegistrantConfig.OUTPUT_FILENAME
+OUTPUT_FILE_PATH = (
+    GlobalConfig.OUTPUT_DIRECTORY_PATH
+    / WebinarsByRegistrantConfig.OUTPUT_FILENAME
+)
 with open(OUTPUT_FILE_PATH, "w") as file:
     csv = webinars_by_registrant.to_csv()
     file.write(csv)
